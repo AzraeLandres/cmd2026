779bd93dc16ccdc3e97884b73a497d20459582b2
Azrael Landres <azraellandres@gmail.com>
2026-07-04 21:01:05 +0200

    ui : ajout alias

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@atoms/*": path.resolve(__dirname, "./src/atoms/*"),
+      "@molecules/*": path.resolve(__dirname, "./src/molecules/*"),
+      "@organisms/*": path.resolve(__dirname, "./src/organisms/*"),
+      "@graphql/*": path.resolve(__dirname, "./src/graphql/*"),
+      "@interfaces/*": path.resolve(__dirname, "./src/interfaces/*"),
+    },
+  },
   server: {
     proxy: {
-      '/graphql': 'http://localhost:3000',
+      "/graphql": "http://localhost:3000",
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
 });
